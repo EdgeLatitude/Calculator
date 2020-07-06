0c5c6931f647b8a4624bb23370095366ea3b45ce
Aldo Ortiz <aldo.ortiz@outlook.com>
2020-07-05 21:57:06 -0500

    Custom picker encapsulation.

--- Calculator.Mobile/Calculator.Mobile/Pages/SettingsPage.xaml.cs
+++ Calculator.Mobile/Calculator.Mobile/Pages/SettingsPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using Calculator.Shared.Localization;
-using Calculator.Shared.ViewModels;
-using System.Linq;
+﻿using Calculator.Shared.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,42 +7,10 @@ namespace Calculator.Mobile.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPage : ContentPage
     {
-        private bool _themePickerIsShowing;
-
         public SettingsPage()
         {
             InitializeComponent();
             BindingContext = ViewModelLocator.Instance.Resolve<SettingsViewModel>();
-        }
-
-        private void ThemePicker_Focused(object sender, FocusEventArgs args)
-        {
-            if (Device.RuntimePlatform != Device.Android)
-                return;
-
-            var themePicker = (Picker)sender;
-            themePicker.Unfocus();
-
-            Device.BeginInvokeOnMainThread(async () =>
-            {
-                if (_themePickerIsShowing)
-                    return;
-
-                _themePickerIsShowing = true;
-
-                var senderPickerItems = themePicker.Items.ToArray();
-
-                var selectedItem = await Application.Current.MainPage.DisplayActionSheet(
-                    null,
-                    LocalizedStrings.Cancel,
-                    null,
-                    senderPickerItems);
-
-                if (senderPickerItems.Contains(selectedItem))
-                    themePicker.SelectedItem = selectedItem;
-
-                _themePickerIsShowing = false;
-            });
         }
     }
 }
