fe70bb216dead5d787e6d805c6ecb75d1460de60
Aldo Ortiz <aldo.ortiz@outlook.com>
2021-01-04 21:25:33 -0600

    Show cursor.

--- Calculator.Mobile/Calculator.Mobile/Converters/IsSelectedConverter.cs
+++ Calculator.Mobile/Calculator.Mobile/Converters/IsSelectedConverter.cs
@@ -4,16 +4,18 @@ using Xamarin.Forms;
 
 namespace Calculator.Mobile.Converters
 {
-    class NegateBooleanConverter : IValueConverter
+    class IsSelectedConverter : IValueConverter
     {
         public object Convert(object value, Type targetType,
             object parameter, CultureInfo culture) =>
             value is bool boolValue ?
-                !boolValue :
-                false;
+                boolValue ?
+                    TextDecorations.Underline :
+                    TextDecorations.None :
+                TextDecorations.None;
 
         public object ConvertBack(object value, Type targetType,
             object parameter, CultureInfo culture) =>
-            Convert(value, targetType, parameter, culture);
+            false;
     }
 }
