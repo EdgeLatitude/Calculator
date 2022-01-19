4e4f787f4f1b5e246a124cb2d501089d0db46683
Aldo Ortiz <aldo.ortiz@outlook.com>
2022-01-18 21:46:27 -0600

    Constants.

--- Calculator.Shared/Constants/Strings.cs
+++ Calculator.Shared/Constants/Strings.cs
@@ -2,9 +2,9 @@
 {
     public static class Strings
     {
-        public const string HistoryLength = "HistoryLength";
-        public const string ResultsHistory = "ResultsHistory";
-        public const string Theme = "Theme";
+        public const string HistoryLength = nameof(HistoryLength);
+        public const string ResultsHistory = nameof(ResultsHistory);
+        public const string Theme = nameof(Theme);
         public const string WhiteSpace = " ";
         public const string HttpsProtocol = "https://";
         public const string SourceCodeWebsite = "github.com/EdgeLatitude/Calculator";
